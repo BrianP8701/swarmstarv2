3b9f865405cd560b59521cfa8943bb5c421cbfa7
Brian Przezdziecki <brianp8701@gmail.com>
2024-09-10 17:36:03 -0700

    fix pub sub functions deployment

--- infra/deploy/functions/deploy-functions.sh
+++ infra/deploy/functions/deploy-functions.sh
@@ -17,25 +17,3 @@ wait
 sh "$DIR/deploy-functions-connect-cloudsql.sh" clerk-event-http $REGION $CLOUDSQL_INSTANCE &
 sh "$DIR/deploy-functions-connect-cloudsql.sh" operation-handler $REGION $CLOUDSQL_INSTANCE &
 wait
-
-# Add this function to check if a handler should be deployed as a cloud function
-should_deploy_as_cloud_function() {
-    local handler_name=$1
-    case $handler_name in
-        WebSocketHandler)
-            return 1  # Don't deploy as a cloud function
-            ;;
-        *)
-            return 0  # Deploy as a cloud function
-            ;;
-    esac
-}
-
-# Modify the deployment loop
-for handler in ${HANDLERS[@]}; do
-    if should_deploy_as_cloud_function $handler; then
-        deploy_function $handler
-    else
-        echo "Skipping deployment of $handler as a cloud function"
-    fi
-done
