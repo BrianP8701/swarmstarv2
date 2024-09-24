22cc29e613a609d6a7964199449aadb137dbbc89
Brian Przezdziecki <brianp8701@gmail.com>
2024-09-24 08:46:48 -0400

    wip

--- server/src/graphql/resolvers/subscriptions/index.ts
+++ server/src/graphql/resolvers/subscriptions/index.ts
@@ -1,10 +1,23 @@
 import { PubSub } from 'graphql-subscriptions';
 import { SubscriptionResolvers } from '../../generated/graphql';
+import { ResolverContext } from '../../createApolloServer';
+import assert from 'assert';
 
 const pubsub = new PubSub();
 
 export const Subscription: SubscriptionResolvers = {
   messageReceived: {
-    subscribe: (_parent, { userId }) => pubsub.asyncIterator(`MESSAGE_RECEIVED_${userId}`),
+    subscribe: async (_, __, { req }: ResolverContext) => {
+      assert(req.user, 'User not found');
+      const asyncIterator = pubsub.asyncIterator(`MESSAGE_RECEIVED_${req.user.id}`);
+      return {
+        [Symbol.asyncIterator]() {
+          return asyncIterator;
+        },
+      };
+    },
+    resolve: (payload: any) => {
+      return payload;
+    },
   },
 };
