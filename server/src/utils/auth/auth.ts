2d15226bfa4cd9562f2a6389d712c7f771ce10cb
Brian Przezdziecki <brianp8701@gmail.com>
2024-09-30 05:29:02 -0400

    debug

--- server/src/utils/auth/auth.ts
+++ server/src/utils/auth/auth.ts
@@ -3,9 +3,15 @@ import { container } from '../di/container';
 import { UserService } from '../../services/UserService';
 import { AuthenticatedRequest } from './AuthRequest';
 import { logger } from '../logging/logger';
+import { SecretService } from '../../services/SecretService';
 
 export const checkAuthenticated = async (req: AuthenticatedRequest, _res: Response, next: NextFunction) => {
   try {
+    const secretService = container.get(SecretService);
+    const isEnvLocal = secretService.getEnvVars().MODE === 'local';
+    if (!isEnvLocal) {
+      logger.info('Authenticating user: ', JSON.stringify(req.auth, null, 2));
+    }
     const sessionClaim = req.auth?.sessionClaims;
     if (sessionClaim) {
       const user = await container.get(UserService).getOrCreateUser(sessionClaim);
